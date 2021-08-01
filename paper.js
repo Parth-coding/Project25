7ef5ac2b08402975698fea98bfd3fd96b4045658
Parth-coding <75926683+Parth-coding@users.noreply.github.com>
2021-08-01 22:26:33 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,41 @@
+class Paper
+{
+    constructor(x,y,r)
+    {
+        var options3={
+		isStatic:false,
+            friction:	0.5,
+            restitution:0.3,
+            density:1.2,		
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		
+		
+		
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options3);
+		this.image = loadImage("paper.png");
+ 		World.add(world, this.body);
+
+		 
+      
+    }
+    display()
+	{
+			
+			var paperPos=this.body.position;		
+
+			push()
+			translate(paperPos.x, paperPos.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			
+		    imageMode(CENTER)
+            image(this.image,0,0,this.r,this.r);
+			this.image.scale=3;
+			
+			pop()
+			
+	}
+}
